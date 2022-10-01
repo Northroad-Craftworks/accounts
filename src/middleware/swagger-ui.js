275a8061ffdb89b52b21d8be5e71fc67f3b22dc8
James Tanner <jftanner@us.ibm.com>
2022-10-01 13:13:21 -0400

    Update: Add simple profile demo

--- src/middleware/swagger-ui.js
+++ src/middleware/swagger-ui.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { apiSpec } from '../lib/api-spec.js';
 import { disableLog, enableLog } from './logger.js';
-import logger from '../lib/logger.js'
 
 const router = new Router();
 export default router;
 
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customCssUrl: '/css/swagger-dark.css'
+}
+
 // Serve the API docs.
 router.use('/api-docs', disableLog, swaggerUi.serve, enableLog);
-router.get('/api-docs', swaggerUi.setup(apiSpec, { customCssUrl: '/css/swagger-dark.css' }));
+router.get('/api-docs', swaggerUi.setup(apiSpec, options));
--- src/operations/whoami.js
+++ src/operations/whoami.js
@@ -1,7 +1,15 @@
-export async function whoami(req, res, next){
-    res.json({
-        user: req.user,
-        session: req.session,
-        accessToken: req.accessToken
-    })
+export async function whoami(req, res, next) {
+    const accepts = req.accepts(['json', 'html']);
+    const { user, session, accessToken } = req;
+    if (accepts === 'html') {
+        res.type(accepts);
+        if (!user) res.send(`<h1>Not logged in</h1>`);
+        else {
+            const name = `<h1>${user.name}</h1>`;
+            const email = `<p>${user.email}</p>`;
+            const photo = `<img src="${user.profilePhoto}" alt="Your profile photo" crossorigin="anonymous" height=100 width=100/>`;
+            res.send(`${name}\n${email}\n${photo}`);
+        }
+    }
+    else res.json({ user, session, accessToken });
 }
